f8687b0715a22e524f7e0b45fd167d6a3223f8e6
Deniz Dogan <denizdogan@ip-192-168-7-139.eu-west-1.compute.internal>
2025-12-15 16:03:07 +0100

    feat: Set German (de) as default language

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,7 +32,7 @@ function LayoutContent({ children }: { children: React.ReactNode }) {
   }
 
   return (
-    <html lang="en">
+    <html lang="de">
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=5" />
       </head>
